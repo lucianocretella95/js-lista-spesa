38f12c5cacd9f9771212c0c303aed50f689c5220
lucianocretella95 <luciano.cretella@tim.it>
2024-04-24 16:26:20 +0200

    M3 corretta posizione elementi array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const spesa = document.querySelector("ul");
+const spesa = document.getElementById("item");
 const lista = ["verdure", "latte", "pane", "pasta", "legumi", "uova"];
 for (let i = 0; i < lista.length; i++) {
   const element = lista[i];
   console.log(i, element);
-  spesa.innerHTML = `<li>${lista}</li>`;
+  spesa.innerHTML += `<li>${i, element}</li>`;
 }
 
 // let i = 0;
