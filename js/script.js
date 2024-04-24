40f05827d06b35da577e250263ec9aa857844f87
lucianocretella95 <luciano.cretella@tim.it>
2024-04-24 15:57:32 +0200

    M2 ciclo while

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const lista = ["verdure", "latte", "pane", "pasta", "legumi", "uova"];
-for (let i = 0; i < lista.length; i++) {
+// for (let i = 0; i < lista.length; i++) {
+//   const element = lista[i];
+//   console.log(i, element);
+// }
+
+let i = 0;
+while (i < lista.length) {
   const element = lista[i];
   console.log(i, element);
+  i++;
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 inizio col creare l'array della lista della spesa che chiamerò: "lista".
 usando un ciclo for itero la lista e stampo in cosole la mia lista.
+oltre al ciclo for posso ottenere lo stesso risultato col ciclo while.
