fb642990e709c0f8541f644acf6915300158c5a6
lucianocretella95 <luciano.cretella@tim.it>
2024-04-24 15:47:27 +0200

    M1 array e ciclo for

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const lista = ["verdure", "latte", "pane", "pasta", "legumi", "uova"];
+for (let i = 0; i < lista.length; i++) {
+  const element = lista[i];
+  console.log(i, element);
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+inizio col creare l'array della lista della spesa che chiamerò: "lista".
+usando un ciclo for itero la lista e stampo in cosole la mia lista.
