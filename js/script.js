65f20c0463c7d43635f8831e99b2fa45f9694f91
lucianocretella95 <luciano.cretella@tim.it>
2024-04-24 16:15:54 +0200

    M3 stampo in pagina col ciclo for

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+const spesa = document.querySelector("ul");
 const lista = ["verdure", "latte", "pane", "pasta", "legumi", "uova"];
-// for (let i = 0; i < lista.length; i++) {
-//   const element = lista[i];
-//   console.log(i, element);
-// }
-
-let i = 0;
-while (i < lista.length) {
+for (let i = 0; i < lista.length; i++) {
   const element = lista[i];
   console.log(i, element);
-  i++;
+  spesa.innerHTML = `<li>${lista}</li>`;
 }
+
+// let i = 0;
+// while (i < lista.length) {
+//   const element = lista[i];
+//   console.log(i, element);
+//   i++;
+// }
